e61b026cd3bbea33eae911a9323e10c801debe33
Paula Mendez Sanchez <paulipotter@users.campus>
2017-06-30 11:55:29 -0500

    test echo command

--- sasa-weather-tool.sh
+++ sasa-weather-tool.sh
@@ -6,8 +6,8 @@
     #     *) echo "no clue - $i"; ;;
    # esac;
 # fi
-zip = "$(60638)"
-location="$(Chicago MidWay)"
+zip = "60638"
+location="Chicago MidWay"
 today="$(date '+%Y%m%d')"
 yesterday="$(date -d yesterday '+%Y%m%d')"
 echo "$location | $yesterday | $zip | TEMP,STP,SPD,DIR,PCP01" > noaa-reqs.txt
