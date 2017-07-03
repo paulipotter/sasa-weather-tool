f2e7bd299cfc695d6068d74751fa4ac5bfd9801b
Paula Mendez <paulipotter@ksu.edu>
2017-07-03 14:34:26 -0500

    pep8 changes

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import psycopg2
 import csv
-cur = conn.cursor()
 
 conn = psycopg2.connect("dbname=template1 user=paulipotter")
+cur = conn.cursor()
 cur.execute(" CREATE TABLE test (idserial PRIMARYKEY, numinteger, data);")
 cur.execute(" INSERT INTO test (num, data) VALUES (%s,%s)", (100, "abcdef"))
 with open('file.csv') as csv_file:
-    rw = csv.reader(csv_file, delimeter=',', quotechar)
     rw = csv.reader(csv_file, delimeter=',')
     rw.next()
-    for row in read_csv:
-        cur.execute("Insert into test (num,data)",row)
+    for row in rw:
+        cur.execute("Insert into test (num,data)", row)
 
 
 # print(cur.fetchone())
 
 
 
-#Make the changes to the database persistent
+# Make the changes to the database persistent
 conn.commit()
 
-#Close communication with the database
+# Close communication with the database
 cur.close()
 conn.close()
