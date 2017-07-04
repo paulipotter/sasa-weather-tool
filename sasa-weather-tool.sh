09b538670a98fcd4c074687641efa460207a7baa
Paula Cecilia Mendez Sanchez <paulipotter@fingolfin.cs.ksu.edu>
2017-07-04 16:29:55 -0500

    noaahist API error pending

--- sasa-weather-tool.sh
+++ sasa-weather-tool.sh
@@ -6,11 +6,14 @@ today="$(date '+%Y%m%d')"
 yesterday="$(date -d yesterday '+%Y%m%d')"
 
 # Write the command and export it
-echo "CHICAGO MIDWAY INTL ARPT | $yesterday | 60638 | DIR,SPD,GUS,CLG,SKC,L,M,H,VSB,TEMP,DEWP,SLP,ALT,STP,MAX,MIN,PCP01,PCP06,PCP24,PCPXX,SD" > noaa-reqs.txt
+echo '''CHICAGO MIDWAY INTL AIRPORT | 20170623
+  | 41.7868, 87.7522 | DIR,SPD,GUS,CLG,SKC,
+  L,M,H,VSB,TEMP,DEWP,SLP,ALT,STP,MAX,MIN,
+  PCP01,PCP06,PCP24,PCPXX,SD''' > noaa-reqs.txt
 echo "Command Exported to noaa-reqs.txt"
 
 # Run the NOAA API
-python3 ../noaahist/noaahist.py --infile noaa-reqs.txt -p -m -o weather-info.csv
+python ./noaahist/noaahist.py --infile noaa-reqs.txt -p -m -o weather-info.csv
 
 #Run python
 # python3 main.py
