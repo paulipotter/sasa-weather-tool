aa19393d21f834d949cfc9ddfd941066342bfea4
Paula Cecilia Mendez Sanchez <paulipotter@fingolfin.cs.ksu.edu>
2017-07-04 13:41:57 -0500

    final database test

--- main.py
+++ main.py
@@ -3,14 +3,34 @@ import csv
 
 conn = psycopg2.connect("dbname=template1 user=postgres")
 cur = conn.cursor()
-# cur.execute(" CREATE TABLE test ( num integer, data varchar(55));")
-cur.execute(" INSERT INTO test (num, data)  VALUES (%s,%s)", (100, "abcdef"))
+
+cur.execute(''' CREATE TABLE testcol 
+            (DIR varchar (50),SPD varchar (50), GUS varchar (50), CLG varchar (50), SKC varchar (50), L varchar (50), 
+            M varchar (50), H varchar(50), VSB varchar(50), TEMP varchar(50), DEWP varchar(50), SLP varchar(50),
+            ALT varchar(50), STP varchar(50), MAX varchar(50), MIN varchar(50), PCP01 varchar(50), PCP06 varchar(50)
+            PCP24 varchar(50), PCPXX varchar(50), SD)
+            
+                VALUES
+                (%s, %s, %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s, %s, %s);''')
+
+print("Opening CSV...")
+
 with open('file.csv') as csv_file:
     rw = csv.reader(csv_file)
     for row in rw:
         print (row)
-        cur.execute("Insert into test (num,data) Values (%s, %s);", row)
-
+        cur.execute(''' Insert into test 
+                        (DIR,SPD,GUS,CLG,SKC,L,M,H,
+                        VSB,TEMP,DEWP,SLP,ALT,STP,
+                        MAX,MIN,PCP01,PCP06,PCP24,PCPXX,SD)
+                        VALUES
+                        (%s, %s, %s, %s, %s, %s, %s,
+                        %s, %s, %s, %s, %s, %s, %s,
+                        %s, %s, %s, %s, %s, %s, %s);''', 
+                       row)
+                    
 
 # print(cur.fetchone())
 
@@ -22,3 +42,4 @@ conn.commit()
 # Close communication with the database
 cur.close()
 conn.close()
+print("Database connection is closed")
--- sasa-weather-tool.sh
+++ sasa-weather-tool.sh
@@ -4,10 +4,11 @@ today="$(date '+%Y%m%d')"
 yesterday="$(date -d yesterday '+%Y%m%d')"
 
 # Write the command and export it
-echo "CHICAGO MIDWAY INTL ARPT | $yesterday | 60638 | TEMP,STP,SPD,DIR,PCP01" > noaa-reqs.txt
+echo "CHICAGO MIDWAY INTL ARPT | $yesterday | 60638 | DIR,SPD,GUS,CLG,SKC,L,M,H,VSB,TEMP,DEWP,SLP,ALT,STP,MAX,MIN,PCP01,PCP06,PCP24,PCPXX,SD" > noaa-reqs.txt
 echo "Command Exported to noaa-reqs.txt"
 
 # Run the NOAA API
 ../noaahist/noaahist.py --infile noaa-reqs.txt -p -m -o weather-info.csv
 
 #Run python
+# python3 main.py
